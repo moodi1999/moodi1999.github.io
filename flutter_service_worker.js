b01a291e611ccd4e2fa74773285ccb2bf5849e73
Ahmadreza Moodi <moodi1378@gmail.com>
2020-03-02 18:58:53 +0330

    init

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,16 +1,42 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets\FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+  "/assets\AssetManifest.json": "c9daad1a9b925a6f142877d03dea35f9",
+"/assets\assets\fonts\DimaWeb.ttf": "1fa34cba4aafe5733a3133a6a219850b",
+"/assets\assets\fonts\Shabnam-Bold.ttf": "6434a8c072c7c2bbc349ccfff4f5c496",
+"/assets\assets\fonts\Shabnam-Light.ttf": "f38fd5507e7435535401ae038755e7a0",
+"/assets\assets\fonts\Shabnam-Medium.ttf": "c4edea41d105d1060a4d9b7bee7798f0",
+"/assets\assets\fonts\Shabnam-Thin.ttf": "60377b61dec191c3bb311d8390207890",
+"/assets\assets\fonts\Shabnam.ttf": "7a43025a9e698087ba086fb63704b554",
+"/assets\assets\images\chat.png": "d8cf95dc7db7ae5ccadfa45b66f94e6f",
+"/assets\assets\images\cut_map.jpg": "6cd0464b8a1d7b4a29d541303f9dc612",
+"/assets\assets\images\empty_view.png": "ea6f5b017524c7113f547a8f569c21da",
+"/assets\assets\images\intro1.png": "ae0fef824dcc6ff7d33ee15e8144fd70",
+"/assets\assets\images\intro2.png": "677fd5939aa2a4fa02b0dc3ff6ac2a07",
+"/assets\assets\images\intro3.png": "bce851fb3c1b0bfbacc097ad06650cf5",
+"/assets\assets\images\main.png": "51ea532c0dac1c3df588198aa0e7785a",
+"/assets\assets\images\map.jpg": "a4c727051874e22fab51d0fabbc36b84",
+"/assets\assets\images\microscope.png": "6a18f6c1bcaa4898daff922dc5cdbef6",
+"/assets\assets\images\nurse.png": "938eb73360c5f3ebd16befe161ef335a",
+"/assets\assets\images\pill.png": "dd051015055e9c91124027a60aedd9b9",
+"/assets\assets\images\profile.png": "0c7814cd8e254f214833efc9d49f9cbd",
+"/assets\assets\images\verification.png": "9580c354b85046246c1d8ffbbf610f31",
+"/assets\FontManifest.json": "0bd90f096b16c1cb8234727814528ec5",
 "/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "f79d5c7bd97003d8a7575e184d52b8b3",
+"/assets\LICENSE": "41e0ba06a9ff8b0ed1bf2fa47b57e21a",
 "/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets\packages\map_launcher\assets\icons\amap.png": "eb0053889bd5a874cf3fa38d8ada078d",
+"/assets\packages\map_launcher\assets\icons\apple.png": "b6abfa26b3b7c7474d91ecf656b7a55e",
+"/assets\packages\map_launcher\assets\icons\baidu.png": "20fca1c586f8c05c06b54aa123b08592",
+"/assets\packages\map_launcher\assets\icons\google.png": "e61e895ab828e6d34dba1bd969d1706a",
+"/assets\packages\map_launcher\assets\icons\waze.png": "f128032892d6e2c606a24c0d6a297f2f",
+"/assets\packages\map_launcher\assets\icons\yandexMaps.png": "6b005a2abb5debbe236e5a0caeba6625",
+"/assets\packages\map_launcher\assets\icons\yandexNavi.png": "734c39c9c55e62ed8cd8107c56a63f3e",
 "/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/index.html": "30ed26cd14d35a62c8df267f39e4e078",
-"/main.dart.js": "ff19fd2dff7ef33f73650fb2cd92b6ba",
-"/manifest.json": "8ea9fe138d87e5c694b8b838cc340020"
+"/index.html": "16e85c1a0dfa0a2ff1698199a8eeb8c7",
+"/main.dart.js": "65475d323b5819486d22f0d1b88826ba",
+"/manifest.json": "f0f83bae2414f6cb34d9b7af0e831e6a"
 };
 
 self.addEventListener('activate', function (event) {
